0a4bbec26215a23a033885e652dc8763ceaa4c3c
sik2 <bickwang123@gmail.com>
2023-12-11 14:16:04 +0900

    1강 프로젝트 설명 및 종료 구현

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+       Scanner sc = new Scanner(System.in);
+
+        System.out.println("== 시스템 시작 ==");
+
+        while (true) {
+            System.out.printf("명령) ");
+            String command = sc.nextLine().trim();
+            
+            if (command.equals("종료")) {
+                break;
+            }
+
+        }
+
+
+
+        sc.close();
     }
 }
