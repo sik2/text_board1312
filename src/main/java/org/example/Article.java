98777dc99712f23ba439d59d0ecb60a4157acf24
sik2 <bickwang123@gmail.com>
2023-12-11 14:25:45 +0900

    2강 등록 구현

---
+++ src/main/java/org/example/Article.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Article {
+    int id;
+    String title;
+    String content;
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -8,12 +10,27 @@ public class Main {
 
         System.out.println("== 시스템 시작 ==");
 
+        List<Article> articleList = new ArrayList<>();
+        int lastId = 1;
+
         while (true) {
             System.out.printf("명령) ");
             String command = sc.nextLine().trim();
-            
+
             if (command.equals("종료")) {
                 break;
+            } else if (command.equals("등록")) {
+                System.out.printf("제목 : ");
+                String title = sc.nextLine();
+                System.out.println("내용 : ");
+                String content = sc.nextLine();
+
+                Article article = new Article(lastId, title, content);
+                articleList.add(article);
+
+                lastId++;
+            } else if (command.equals("목록")) {
+                System.out.println(articleList.size());
             }
 
         }
