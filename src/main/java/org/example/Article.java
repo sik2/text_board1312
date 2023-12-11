f08beae852879c3e96578f10263d5534a77d580d
sik2 <bickwang123@gmail.com>
2023-12-11 14:43:13 +0900

    수정구현

--- src/main/java/org/example/Article.java
+++ src/main/java/org/example/Article.java
@@ -2,9 +2,11 @@ package org.example;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
+@Setter
 public class Article {
     int id;
     String title;
