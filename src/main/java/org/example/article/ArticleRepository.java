010a5e84fcbbaf8de2870950b96d6a64c5607e01
sik2 <bickwang123@gmail.com>
2023-12-12 18:14:41 +0900

    article, Service, repository 분리

--- src/main/java/org/example/article/ArticleRepository.java
+++ src/main/java/org/example/article/ArticleRepository.java
@@ -1,5 +1,46 @@
 package org.example.article;
 
-public class ArticleRepository {
+import org.example.Global;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArticleRepository {
+    List<Article> articleList = new ArrayList<>();
+    int lastArticleId = 1;
+
+    public int save (String title, String content) {
+        Article article = new Article(lastArticleId, title, content, Global.getLoginedMember().getUserId(), Global.nowDateTime());
+        articleList.add(article);
+
+        lastArticleId++;
+
+        return article.getId();
+    }
+
+    public List<Article> findByAll() {
+        return articleList;
+    }
+
+    public int delete(Article article) {
+        articleList.remove(article);
+
+        return article.getId();
+    }
+
+    public int update(Article article, String title, String content) {
+        article.setTitle(title);
+        article.setContent(content);
+
+        return article.getId();
+    }
+
+    public Article articleFindById(int id) {
+        for (int i = 0; i < articleList.size(); i++) {
+            if (id == articleList.get(i).getId()) {
+                return articleList.get(i);
+            }
+        }
+        return null;
+    }
 }
--- src/main/java/org/example/article/ArticleService.java
+++ src/main/java/org/example/article/ArticleService.java
@@ -1,47 +1,31 @@
 package org.example.article;
 
-import org.example.Global;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class ArticleService {
-    List<Article> articleList = new ArrayList<>();
-    int lastArticleId = 1;
+    private ArticleRepository articleRepository;
+
+    ArticleService () {
+        articleRepository = new ArticleRepository();
+    }
 
     public int save (String title, String content) {
-        Article article = new Article(lastArticleId, title, content, Global.getLoginedMember().getUserId(), Global.nowDateTime());
-        articleList.add(article);
-
-        lastArticleId++;
-
-        return article.getId();
+        return this.articleRepository.save(title, content);
     }
 
     public List<Article> findByAll() {
-        return articleList;
+        return this.articleRepository.findByAll();
     }
 
     public int delete(Article article) {
-        articleList.remove(article);
-
-        return article.getId();
+        return this.articleRepository.delete(article);
     }
 
     public int update(Article article, String title, String content) {
-        article.setTitle(title);
-        article.setContent(content);
-
-        return article.getId();
+        return this.articleRepository.update(article, title, content);
     }
 
     public Article articleFindById(int id) {
-        for (int i = 0; i < articleList.size(); i++) {
-            if (id == articleList.get(i).getId()) {
-                return articleList.get(i);
-            }
-        }
-        return null;
+        return this.articleRepository.articleFindById(id);
     }
-
 }
